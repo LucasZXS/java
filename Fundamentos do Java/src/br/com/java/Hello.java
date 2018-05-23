f87030f894cdb8e026cb1711e676a4351bf9635a
LucasZXS <38111076+LucasZXS@users.noreply.github.com>
2018-05-23 16:22:59 -0300

    Adicionado arquivos via upload

---
+++ Fundamentos do Java/src/br/com/java/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.java;
+
+/**
+ * @author lucas.svieira4
+ *
+ */
+public class Hello {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Lucas Soares Vieira");
+
+	}
+
+}
